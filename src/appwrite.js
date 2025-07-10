4453c542273d9908b8901d27d4db8a50249a9c3d
Saksham Goel <sakshamgoel1107@gmail.com>
2025-07-10 16:06:16 +0530

    Update

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,11 +1,11 @@
-import { Client, ID, InputFile, Storage } from 'node-appwrite';
+import { Client, ID, InputFile, Storage, Databases } from 'node-appwrite';
 
 class AppwriteService {
   constructor() {
     const client = new Client();
     client
       .setEndpoint(
-        process.env.APPWRITE_ENDPOINT ?? 'https://<REGION>.cloud.appwrite.io/v1'
+        process.env.APPWRITE_ENDPOINT ?? 'https://nyc.cloud.appwrite.io/v1'
       )
       .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
       .setKey(process.env.APPWRITE_API_KEY);
@@ -15,7 +15,22 @@ class AppwriteService {
   }
 
   async createFile(bucketId, blob) {
-    const file = await InputFile.fromBuffer(blob, 'audio.flac');
+    let file;
+    
+    if (blob instanceof Buffer) {
+      file = InputFile.fromBuffer(blob, 'audio.flac');
+    } else if (blob instanceof Uint8Array) {
+      file = InputFile.fromBuffer(Buffer.from(blob), 'audio.flac');
+    } else if (blob instanceof Blob) {
+      const arrayBuffer = await blob.arrayBuffer();
+      const buffer = Buffer.from(arrayBuffer);
+      file = InputFile.fromBuffer(buffer, 'audio.flac');
+    } else if (typeof blob === 'string') {
+      file = InputFile.fromBuffer(Buffer.from(blob), 'audio.flac');
+    } else {
+      file = InputFile.fromBuffer(Buffer.from(blob), 'audio.flac');
+    }
+    
     return await this.storage.createFile(bucketId, ID.unique(), file);
   }
 }
